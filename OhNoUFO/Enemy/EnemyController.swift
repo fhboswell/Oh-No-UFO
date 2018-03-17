10d33fe4fc5e2afd693d42d0f4f1b4884713b9f7
fhboswell <franklin.henry.boswell@gmail.com>
2018-03-16 23:22:49 -0700

    refactor test after issue

--- OhNoUFO/Enemy/EnemyController.swift
+++ OhNoUFO/Enemy/EnemyController.swift
@@ -145,7 +145,8 @@ class EnemyController{
             let action = SCNAction.rotateBy(x: 0, y: CGFloat(random + 10), z: 0, duration: 20)
             ufoNode.runAction(action)
         }
-        
+        //test
+        //test2
         
         
         enemyNodes.append(ufoNode)
